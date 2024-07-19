8df32ccab30b8ec05ef3354654da46de8a158d7e
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-07-19 15:27:42 +0530

    Html parsing & prettify the soup

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://timesofindia.indiatimes.com/city/delhi"
 
 r = requests.get(url)
+
+print(r.status_code)
 print(r.text)
 print(r.url)
 print(r.status_code)
+
+soup = BeautifulSoup(r.text , "html.parser")
+print(soup.prettify())
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+print(soup.title.parent.name)
+print(soup.p)
+print(soup.a)
+print(soup.h1)
